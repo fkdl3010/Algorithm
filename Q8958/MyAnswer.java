32f0537b9ddc4d353853eda353c9dee6147f361c
fkdl3010 <fkdl3919@icloud.com>
2021-03-15 02:42:09 +0900

    answer

---
+++ Q8958/MyAnswer.java
@@ -0,0 +1,38 @@
+package Q8958;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MyAnswer {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		ArrayList<String> list = new ArrayList<String>();
+		int n = sc.nextInt();
+		int[] answer = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			
+			list.add(sc.next());
+		}
+		sc.close();
+		for(String temp: list) {
+			int m = 0;
+			int house = 0;
+			for (int i = 0; i < temp.length(); i++) {
+				if(temp.charAt(i) == 'O') {
+					house++;
+					
+				}else {
+					house = 0;
+				}
+				m += house;
+
+			}
+		}
+		
+	}
+
+}
