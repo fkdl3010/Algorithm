af2ae9fb7aae4bc5b1ca7223204c8b272bcfe331
fkdl3010 <fkdl3919@icloud.com>
2021-04-01 23:45:29 +0900

    [2021-04-01]

--- Q5622/MyAnswer.java
+++ Q5622/MyAnswer.java
@@ -1,5 +1,28 @@
 package Q5622;
 
-public class MyAnswer {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class MyAnswer {
+	
+	public static void main(String[] args) throws IOException{
+		//https://www.acmicpc.net/problem/5622
+		// 2021-04-01
+		
+		String[] alpa = {"ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String read = br.readLine();
+		int answer = 0;
+		for (int i = 0; i < read.length(); i++) {
+			for (int j = 0; j < alpa.length; j++) {
+				if(alpa[j].indexOf(read.charAt(i)) >= 0) {
+					answer += j + 3;
+				};
+			}
+		}
+		System.out.println(answer);
+	}
 }
