695af27468536078038efba063cba8bca49f2979
fkdl3010 <fkdl3919@icloud.com>
2021-04-06 23:53:49 +0900

    2021-04-06

--- Q2941/MyAnswer.java
+++ Q2941/MyAnswer.java
@@ -1,7 +1,110 @@
 package Q2941;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class MyAnswer {
-		public static void main(String[] args) {
+		public static void main(String[] args) throws Exception{
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			String target = br.readLine();
+			int count = 0;
+			char ch = ' ';
+			for(int i = 0; i < target.length(); i++) {
+				
+				ch = target.charAt(i);
+				
+				if(ch == 'c') {
+					
+					
+					if(i < target.length() - 1) {
+						
+						if(target.charAt(i + 1) == '=') {
+							i++;
+						}
+						else if(ch == '-') {
+							i++;
+						}
+					}
+					
+				}
+				
+				if(ch == 'd') {
+					if(i < target.length() - 1) {
+						
+						if(target.charAt(i + 1) == 'z') {
+							
+							if(i < target.length() - 2) {
+								
+								if(target.charAt(i + 1) == '=') {
+									
+									i += 2;
+								} 
+							}
+						}
+						else if(target.charAt(i + 1) == '-') {
+							i++;
+						}
+					}
+					
+				}
+				
+				if(ch == 'l') {
+					
+					
+					if(i < target.length() - 1) {
+						
+						if(target.charAt(i + 1) == 'j') {
+							i++;
+						}
+						
+					}
+					
+				}
+				
+				if(ch == 'n') {
+					
+					
+					if(i < target.length() - 1) {
+						
+						if(target.charAt(i + 1) == 'j') {
+							i++;
+						}
+						
+					}
+					
+				}
+				
+				if(ch == 's') {
+					
+					
+					if(i < target.length() - 1) {
+						
+						if(target.charAt(i + 1) == '=') {
+							i++;
+						}
+						
+					}
+					
+				}
+				
+				if(ch == 'z') {
+					
+					
+					if(i < target.length() - 1) {
+						
+						if(target.charAt(i + 1) == '=') {
+							i++;
+						}
+						
+					}
+					
+				}
+				count++;
+			}
+			
+			System.out.println(count);
 			
 		}
 }
