4fcc9c4885972801ff66a4ceffae3e096df52435
fkdl3010 <fkdl3919@icloud.com>
2021-04-08 23:13:10 +0900

    2021-04-08

---
+++ Q1316/MyAnswer.java
@@ -0,0 +1,34 @@
+package Q1316;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+
+public class MyAnswer {
+
+	public static void main(String[] args) throws Exception{
+		// https://www.acmicpc.net/problem/1316
+		// 2021-04-08
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = br.read();
+		br.readLine();
+		int count = 0;
+		String[] s = new String[n];
+		for(int i = 0; i < n; i++) {
+			s[i] = br.readLine();
+		}
+		
+		for(int i = 0; i < n; i++) {
+			
+			for (int j = 0; j < s[i].length(); j++) {
+				
+				
+			}
+			
+		}
+		
+	}
+
+}
