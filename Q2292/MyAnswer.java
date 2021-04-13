c7b55d2ef22346f4a40e4d42d6526eb5871621fe
fkdl3010 <fkdl3919@icloud.com>
2021-04-14 00:38:51 +0900

    2021-04-13

--- Q2292/MyAnswer.java
+++ Q2292/MyAnswer.java
@@ -1,5 +1,36 @@
 package Q2292;
 
-public class MyAnswer {
+import java.util.Scanner;
 
+public class MyAnswer {
+	
+	public static void main(String[] args) {
+		// https://www.acmicpc.net/problem/2292
+		// 2021-04-13
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int count = 1;
+		int range = 2;
+		
+		if(n == 1) {
+			System.out.println(1);
+		}else {
+			
+			while( range <= n ) {
+				
+				range = range + (6 * count);
+				count++;
+				
+			}
+			
+			System.out.println(count);
+			
+		}
+		sc.close();
+		
+		
+	}
 }
