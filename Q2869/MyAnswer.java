7f35f6aee66c62a822d1d7f5d89a72595487d74d
fkdl3010 <fkdl3919@icloud.com>
2021-04-20 23:38:16 +0900

    2021-04-19

--- Q2869/MyAnswer.java
+++ Q2869/MyAnswer.java
@@ -1,5 +1,24 @@
 package Q2869;
 
-public class MyAnswer {
+import java.util.Scanner;
 
+public class MyAnswer {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int mount = (c - b) / (a-b);
+		
+		if( (c - b) % (a - b) != 0 ) {
+			mount++;
+		}
+		
+		
+		System.out.println(mount);
+		sc.close();
+	}
 }
